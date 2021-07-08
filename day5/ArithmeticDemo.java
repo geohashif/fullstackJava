48f532aca4e68d9d32a0c62d179b8a66ab5a126f
geohashif <71051873+geohashif@users.noreply.github.com>
2021-07-08 16:55:51 +0530

    Add files via upload

---
+++ day5/ArithmeticDemo.java
@@ -0,0 +1,32 @@
+package hit.day5;
+
+import java.util.Scanner;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Please enter i value..:");
+		int i=scan.nextInt();
+		
+		System.out.println("Please enter j value...:");
+		int j=scan.nextInt();
+		
+		int add=i+j;
+		System.out.println("Addtion ....:"+add);
+		
+		int sub=i-j;
+		System.out.println("Subtraction..:"+sub);
+		
+		int mul=i*j;
+		System.out.println("Multiplication...:"+mul);
+		
+		int div=i/j;
+		System.out.println("division...:"+div);//quotient
+		
+		int rem=i%j;
+		System.out.println("Reminder...:"+rem);//reminder
+	}
+
+}
